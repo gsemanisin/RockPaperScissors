cf91ffdb8ed1de94c46ee80b49df36954b3db080
bfehr11 <bfehr11@gmail.com>
2019-05-20 14:46:47 -0500

    image picker

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -9,41 +9,74 @@
 import UIKit
 import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet weak var computersChoiceLabel: UILabel!
     @IBOutlet weak var winnerLabel: UILabel!
-    
     @IBOutlet weak var countdownLabel: UILabel!
-    
     @IBOutlet weak var startButton: UIButton!
-    
+    @IBOutlet weak var rockImage: UIImageView!
+    @IBOutlet weak var paperImage: UIImageView!
+    @IBOutlet weak var scissorsImage: UIImageView!
     
     var choices = ["Rock", "Paper", "Scissors"]
     var playersChoice : String?
     var computersChoice : String?
-    
     var timer: Timer?
     var timeLeft = 3
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let rockTap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+        rockTap.numberOfTapsRequired = 2
+        rockImage.addGestureRecognizer(rockTap)
+        let paperTap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+        paperTap.numberOfTapsRequired = 2
+        paperImage.addGestureRecognizer(paperTap)
+        let scissorsTap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+        scissorsTap.numberOfTapsRequired = 2
+        scissorsImage.addGestureRecognizer(scissorsTap)
+        
     }
 
     @IBAction func rockPressed(_ sender: Any) {
         playersChoice = choices[0]
-        print(playersChoice)
+        timer?.invalidate()
+        timer = nil
+        startButton.isEnabled = true
+        timeLeft = 3
         whoWins()
     }
     
     @IBAction func paperPressed(_ sender: Any) {
         playersChoice = choices[1]
+        timer?.invalidate()
+        timer = nil
+        startButton.isEnabled = true
+        timeLeft = 3
         whoWins()
     }
     
     @IBAction func scissorsPressed(_ sender: Any) {
         playersChoice = choices[2]
+        timer?.invalidate()
+        timer = nil
+        startButton.isEnabled = true
+        timeLeft = 3
         whoWins()
+    }
+    
+    @objc func doubleTapped() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            
+            var myPickerController = UIImagePickerController()
+
+            myPickerController.delegate = self
+            myPickerController.sourceType = .savedPhotosAlbum
+            myPickerController.allowsEditing = false
+            
+            present(myPickerController, animated: true, completion: nil)
+        }
     }
     
     func whoWins() {
@@ -111,9 +144,16 @@ class ViewController: UIViewController {
             timer?.invalidate()
             timer = nil
             startButton.isEnabled = true
+            whoWins()
             timeLeft = 3
         }
         
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        rockImage.image = image
+        dismiss(animated:true, completion: nil)
     }
     
 }
