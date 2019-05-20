3efc44c39c35b95d98f5614f17642a70c96ab1bd
bfehr11 <bfehr11@gmail.com>
2019-05-20 13:14:35 -0500

    first 15 mins

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var computersChoiceLabel: UILabel!
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    var choices = ["Rock", "Paper", "Scissors"]
+    var playersChoice : String?
+    var computersChoice : String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rockPressed(_ sender: Any) {
+        playersChoice = choices[0]
+        
+        whoWins()
+    }
+    
+    @IBAction func paperPressed(_ sender: Any) {
+        playersChoice = choices[1]
+    }
+    
+    @IBAction func scissorsPressed(_ sender: Any) {
+        playersChoice = choices[2]
+    }
+    
+    func whoWins() {
+        let choice = CGFloat.random(in: (0...2))
+        computersChoice = choices[Int(choice)]
+        var winner = ""
+        
+        switch playersChoice {
+        case "Rock":
+            if computersChoice == "Rock" {
+                winner = "tie"
+            }
+            else if computersChoice == "Paper" {
+                winner = "computer"
+            }
+            else {
+                winner = "player"
+            }
+        default:
+            break
+        }
+    }
+    
 }
 
